f411da30c78ac72a08b815ea91d9f73fb187da7e
zhangjie <zhangjie@baiwenbao.com>
2016-03-22 16:41:06 +0800

    Initial Commit

---
+++ ExampleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExampleDemo
+//
+//  Created by ZhangJie on 16/3/22.
+//  Copyright © 2016年 zj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
